dcddaf021e5861c97945f47fa5919ab7cf3b828f
ronaksince2001 <theronaks@gmail.com>
2022-12-01 22:37:21 +0530

    change 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,41 @@
 'use strict';
 
-
-
 /**
  * navbar toggle
  */
 
-const navOpenBtn = document.querySelector("[data-nav-open-btn]");
-const navbar = document.querySelector("[data-navbar]");
-const navCloseBtn = document.querySelector("[data-nav-close-btn]");
-const overlay = document.querySelector("[data-overlay]");
-
-const elemArr = [navCloseBtn, overlay, navOpenBtn];
-
-for (let i = 0; i < elemArr.length; i++) {
-  elemArr[i].addEventListener("click", function () {
-    navbar.classList.toggle("active");
-    overlay.classList.toggle("active");
-  });
-}
-
-/**
- * toggle navbar & overlay when click any navbar-link
- */
-
-const navbarLinks = document.querySelectorAll("[data-navbar-link]");
-
-for (let i = 0; i < navbarLinks.length; i++) {
-  navbarLinks[i].addEventListener("click", function () {
-    navbar.classList.toggle("active");
-    overlay.classList.toggle("active");
-  });
-}
-
-
-
-
-
-/**
- * header & go-top-btn active
- * when window scroll down to 400px
- */
-
 const header = document.querySelector("[data-header]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const navLinks = document.querySelectorAll("[data-navbar-link]");
+
+navToggleBtn.addEventListener("click", function () {
+  this.classList.toggle("active");
+  header.classList.toggle("nav-active");
+});
+
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click", function () {
+    header.classList.toggle("nav-active");
+    navToggleBtn.classList.toggle("active");
+  });
+}
+
+
+
+/**
+ * header scroll active state & go to top
+ */
+
 const goTopBtn = document.querySelector("[data-go-top]");
 
 window.addEventListener("scroll", function () {
-  if (window.scrollY >= 400) {
+
+  if (window.scrollY >= 100) {
     header.classList.add("active");
     goTopBtn.classList.add("active");
   } else {
     header.classList.remove("active");
     goTopBtn.classList.remove("active");
   }
+
 });
